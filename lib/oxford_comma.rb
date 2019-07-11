264e357fda699c1da92df89596dfe4d7238c7d49
Andres Carvajal <jc1993box@hotmail.com>
2019-07-11 20:04:19 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    return array[0]
+  elsif array.count == 2
+    return array.join(" and ")
+  else array.count > 2
+    last=array.pop
+     return array.join(", ") + ", and " + last
+  end
 end
